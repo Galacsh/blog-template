6358a4e2cdcffe09050809415143ef8135db99d1
Galacsh <galacsh.dev@gmail.com>
2024-05-19 02:26:12 +0900

    fix: padding around logo

--- components/GlobalHeader.tsx
+++ components/GlobalHeader.tsx
@@ -6,11 +6,11 @@ const GlobalHeader = () => {
   return (
     <header className="h-16 border-b border-black-100 dark:border-white-100 flex flex-col justify-around items-center">
       <div className="flex flex-1 w-full items-center max-w-screen-2xl">
-        <nav className="flex items-center gap-8">
-          <div className="flex items-center hover:opacity-50 hover:cursor-pointer pr-2">
+        <nav className="flex items-center gap-8 px-4">
+          <div className="flex items-center hover:opacity-50 hover:cursor-pointer">
             <Image src={appIcon} alt="App Icon" className="size-8" />
-            <div className="h-4 w-[1px] bg-white dark:bg-white-300 ml-0.5 mr-2" />
-            <span className="font-mono font-black text-black dark:text-white">
+            <div className="h-4 w-[1px] bg-white dark:bg-white-300 ml-0.5" />
+            <span className="font-mono font-black text-black dark:text-white px-2">
               Galacsh
             </span>
           </div>
