ed20d563d4b9d87f63a18c01ff1dc57619d03c3b
Galacsh <galacsh.dev@gmail.com>
2024-05-20 18:28:01 +0900

    feat: posts page placeholder

---
+++ app/posts/page.tsx
@@ -0,0 +1,12 @@
+export default function PostsPage() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="container p-4">
+        <ul className="list-inside list-disc">
+          <li>TODO: Posts</li>
+          <li>TODO: Posts filter</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
