0b2ef0a304b0dd7eb934402cf6a8f1a585fc5b2e
Galacsh <galacsh.dev@gmail.com>
2024-05-26 00:25:11 +0900

    temp: 'home' page to do list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function HomePage() {
+export default function Home() {
   return (
-    <>
-      <p>Home</p>
-    </>
+    <div className="py-4">
+      <ul className="list-inside list-disc">
+        <li>TODO: Who am I?</li>
+        <li>TODO: Resume link</li>
+        <li>TODO: Links: GitHub, LinkedIn</li>
+        <li>TODO: Go to posts</li>
+        <li>TODO: Recent posts (10)</li>
+      </ul>
+    </div>
   )
 }
