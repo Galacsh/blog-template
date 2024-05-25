939b0ed551f9587bbfee63cdf34b6ee523b4e1a8
Galacsh <galacsh.dev@gmail.com>
2024-05-26 00:17:46 +0900

    style: w-, h- to size-

--- components/theme-toggle-button.tsx
+++ components/theme-toggle-button.tsx
@@ -17,7 +17,7 @@ export function ThemeToggleButton() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="h-8 w-8">
+        <Button variant="outline" size="icon" className="size-8">
           <SunIcon className="absolute h-4 w-4 inline-block dark:hidden" />
           <MoonIcon className="absolute h-4 w-4 hidden dark:inline-block" />
           <span className="sr-only">Toggle theme</span>
