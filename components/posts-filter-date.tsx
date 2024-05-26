2754f32823778451533cf9b50cb83c3f9c7417b5
Galacsh <galacsh.dev@gmail.com>
2024-05-26 20:52:40 +0900

    feat: date filter error message to toast

--- components/posts-filter-date.tsx
+++ components/posts-filter-date.tsx
@@ -8,32 +8,57 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import { FilterLabel } from '@/components/posts-filter-label'
 import { format } from 'date-fns/format'
 import { cn } from '@/lib/utils'
+import { useToast } from './ui/use-toast'
+import { SelectSingleEventHandler } from 'react-day-picker'
 
 export function DateFilter() {
   const [from, setFrom] = useState<Date>()
   const [to, setTo] = useState<Date>()
+  const { toast } = useToast()
 
-  useEffect(() => {
-    if (from && to && from > to) {
-      setTo(undefined)
+  const safeSelectTo = (date: Date | undefined) => {
+    if (from && date && from > date) {
+      toast({
+        title: 'Invalid Date',
+        description: "'To' cannot be earlier than 'From'.",
+      })
+      return false
+    } else {
+      setTo(date)
+      return true
     }
-  }, [from, to])
+  }
+
+  const safeSelectFrom = (date: Date | undefined) => {
+    if (to && date && to < date) {
+      toast({
+        title: 'Invalid Date',
+        description: '"From" cannot be later than "To".',
+      })
+      return false
+    } else {
+      setFrom(date)
+      return true
+    }
+  }
 
   return (
     <div className="w-full grid grid-cols-2 gap-4">
       <FilterDatePicker
         id="from"
         label="From"
-        selected={from}
-        onSelect={setFrom}
         placeholder="Pick a date"
+        align="start"
+        selected={from}
+        onSelect={safeSelectFrom}
       />
       <FilterDatePicker
         id="to"
         label="To"
-        selected={to}
-        onSelect={setTo}
         placeholder="Pick a date"
+        align="end"
+        selected={to}
+        onSelect={safeSelectTo}
       />
     </div>
   )
@@ -43,15 +68,31 @@ type FilterDatePickerProps = Readonly<{
   id: string
   label: string
   placeholder: string
+  align: 'start' | 'end'
   selected: Date | undefined
-  onSelect: (selected: Date | undefined) => void
+  onSelect: (selected: Date | undefined) => boolean
 }>
 
-function FilterDatePicker({ id, label, placeholder, selected, onSelect }: FilterDatePickerProps) {
+function FilterDatePicker({
+  id,
+  label,
+  placeholder,
+  align,
+  selected,
+  onSelect,
+}: FilterDatePickerProps) {
+  const [isOpen, setOpen] = useState(false)
+
+  const easyOnSelect: SelectSingleEventHandler = (day) => {
+    if (onSelect(day)) {
+      setOpen(false)
+    }
+  }
+
   return (
     <div className="flex flex-col">
       <FilterLabel htmlFor={id}>{label}</FilterLabel>
-      <Popover>
+      <Popover open={isOpen} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
             id={id}
@@ -65,8 +106,8 @@ function FilterDatePicker({ id, label, placeholder, selected, onSelect }: Filter
             {selected ? <span>{format(selected, 'PPP')}</span> : <span>{placeholder}</span>}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar mode="single" selected={selected} onSelect={onSelect} />
+        <PopoverContent className="w-auto p-0" align={align}>
+          <Calendar mode="single" selected={selected} onSelect={easyOnSelect} />
         </PopoverContent>
       </Popover>
     </div>
