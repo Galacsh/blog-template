237b681d9b9b1e785fdabfe6bd9f45a6273f0fc7
Galacsh <galacsh.dev@gmail.com>
2024-06-10 16:24:20 +0900

    feat: more env variables

--- env.d.ts
+++ env.d.ts
@@ -6,7 +6,13 @@ declare global {
       APP_DESCRIPTION: string
       BASE_URL: string
       NEXT_PUBLIC_AUTHOR: string
+      NEXT_PUBLIC_ABOUT: string
+      NEXT_PUBLIC_CONTACT: string
+      NEXT_PUBLIC_KEYWORDS: string
+      NEXT_PUBLIC_GITHUB?: string
+      NEXT_PUBLIC_LINKEDIN?: string
       NEXT_PUBLIC_COPYRIGHT_RANGE: string
+      POSTS_PATH: string
     }
   }
 }
