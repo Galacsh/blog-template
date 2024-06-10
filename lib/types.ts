2c60bbff2587e66addb267d56ef5a40365b273b8
Galacsh <galacsh.dev@gmail.com>
2024-06-10 16:33:57 +0900

    feat: update types

--- lib/types.ts
+++ lib/types.ts
@@ -1,17 +1,39 @@
-export type Post = {
+import { JSX } from 'react'
+
+export type Slug = {
+  slug: {
+    full: string
+    arr: string[]
+  }
+}
+
+export type Content = {
+  toc: OnThisPage[]
+  content: JSX.Element
+}
+
+export type RelatedPost = Pick<Frontmatter, 'title'> & Slug
+
+export type AdditionalInfo = {
+  relatedPosts: RelatedPost[]
+}
+
+export type Frontmatter = {
   title: string
   description: string
-  slug: string
-  date?: Date
+  date?: string
   tags?: Tag[]
 }
 
-export type PostPreview = Pick<Post, 'title' | 'description' | 'slug' | 'date' | 'tags'>
+export type Preview = Frontmatter & Slug
 
-export type PostPreviewResponse = {
-  posts: (Pick<Post, 'title' | 'description' | 'slug' | 'tags'> & {
-    date?: string
-  })[]
+export type PostInfo = Preview & AdditionalInfo
+
+export type Post = PostInfo & Content
+
+export type PostTree = {
+  post?: PostInfo
+  children?: Record<string, PostTree>
 }
 
 export type Tag = string
@@ -19,3 +41,25 @@ export type Tag = string
 export type SortBy = 'date' | 'title'
 
 export type SortOrder = 'desc' | 'asc'
+
+export type OnThisPage = {
+  id: string
+  depth: number
+  text: string
+}
+
+export type PreviewResponse = {
+  posts: Preview[]
+}
+
+export type PreviewDateParsed = Omit<Preview, 'date'> & {
+  date: Date | undefined
+}
+
+declare module 'vfile' {
+  interface DataMap {
+    slug: Slug['slug']
+    matter: Frontmatter
+    toc: OnThisPage[]
+  }
+}
