7cbc15a8569c06bde91df0cc2c20789ad7bd4eba
Galacsh <galacsh.dev@gmail.com>
2024-06-11 15:50:02 +0900

    fix: properly handle internal src

--- components/markdown/image.tsx
+++ components/markdown/image.tsx
@@ -1,13 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 
 import type { ComponentProps } from 'react'
+import { config } from '@/lib/config'
 
 export function Image({ alt, src }: ComponentProps<'img'>) {
+  if (src == null) throw new Error("Image 'src' is required.")
+
+  let source = src.includes('://') ? src : internalSrc(src)
+
   return (
     <img
       alt={alt as string}
-      src={src as string}
+      src={source}
       className="max-h-[75vh] sm:shadow-xl rounded-md object-contain"
     />
   )
+}
+
+function internalSrc(src: string) {
+  let parts = src.split('/')
+  parts.unshift(config.basePath)
+  return parts.join('/')
 }
