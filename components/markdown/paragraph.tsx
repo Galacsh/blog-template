897742a986530fe9abc7cd2c0f184707176ed45b
Galacsh <galacsh.dev@gmail.com>
2024-06-16 00:39:32 +0900

    fix: better cjk readability

--- components/markdown/paragraph.tsx
+++ components/markdown/paragraph.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps } from 'react'
 
 export function Paragraph({ children }: ComponentProps<'p'>) {
-  return <p className="mb-4">{children}</p>
+  return <p className="mb-4 break-words break-keep">{children}</p>
 }
